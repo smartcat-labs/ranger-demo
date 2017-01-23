83bf413aa014ee1a8c165fe270339ca136a80a7f
Milan Milosevic <milan.milosevic@smartcat.io>
2017-01-23 11:42:21 +0100

    Start

--- src/test/java/io/smartcat/service/MeasurementServiceTest.java
+++ src/test/java/io/smartcat/service/MeasurementServiceTest.java
@@ -24,67 +24,24 @@ public class MeasurementServiceTest {
 	
 	@Autowired
 	private MongoOperations mongoOps;
-
+	
 	@Test
-	public void getNewestMeasurementsForUser_shouldReturnOnlyUserMeasurementData_forUsername() {
-		createTestMeasurementData();
-		List<Measurement> result = measurementService.getNewestMeasurementsForUser("batman", 50);
-		Assert.assertEquals(50, result.size());
-		result.forEach(m -> Assert.assertEquals("batman", m.getOwner()));
+	public void getNewestMeasurementsForUser_shouldReturnOnlyMeasurementData_forPassedUsername() {
 	}
 	
 	@Test
 	public void getNewestMeasurementsForUser_shouldReturnDataInDescendingOrder() {
-		createTestMeasurementData();
-		List<Measurement> result = measurementService.getNewestMeasurementsForUser("batman", 50);
-		Assert.assertEquals(50, result.size());
-		
-		long previousTimestamp = result.get(0).getCreated();
-		for (Measurement measurement : result) {
-			System.out.println("timestamp is: " + previousTimestamp);
-			Assert.assertTrue(measurement.getCreated() <= previousTimestamp);
-			
-			previousTimestamp = measurement.getCreated();
-		}
-		
 	}
 	
 	@Test
-	public void getNewestMeasurementsForUser_shouldReturn50NewestMeasurments_forBatman() {
-		createTestMeasurementData();
-		List<Measurement> result = measurementService.getNewestMeasurementsForUser("batman", 50);
-		Assert.assertEquals(50, result.size());
-		result.forEach(measurment -> Assert.assertEquals("marker sensor", measurment.getSensor()));
+	public void getNewestMeasurementsForUser_shouldReturn50NewestMeasurments_forPassedUsername() {
 	}
 	
-	private void createTestMeasurementData() {
-		RandomBuilder<Measurement> dataBuilderForOtherUsers = new RandomBuilder<>(Measurement.class);
-		dataBuilderForOtherUsers.randomFrom("owner", "robin", "yoda", "jedi", "skywalker")
-			.randomFromRange("created", 1000L, 2000L) // TODO: fill other properties as well
-			.toBeBuilt(500);
-		
-		RandomBuilder<Measurement> batmanDataBuilder = new RandomBuilder<>(Measurement.class);
-		batmanDataBuilder.randomFrom("owner", "batman")
-			.randomFromRange("created", 1500L, 1550L)
-			.randomFrom("sensor", "marker sensor")
-			.toBeBuilt(50);
-		
-		RandomBuilder<Measurement> oldDataForBatman = new RandomBuilder<>(Measurement.class);
-		oldDataForBatman.randomFrom("owner", "batman")
-			.randomFromRange("created", 1000L, 1500L)
-			.toBeBuilt(500);
-		
-		BuildRunner<Measurement> runner = new BuildRunner<>();
-		runner.addBuilder(dataBuilderForOtherUsers);
-		runner.addBuilder(batmanDataBuilder);
-		runner.addBuilder(oldDataForBatman);
-		
-		List<Measurement> testMeasurements = runner.build();
-		
-		Collections.shuffle(testMeasurements);
-		
-		mongoOps.insertAll(testMeasurements);
-		
+	private void createTestData() {
+		// user: "batman", created : [1500,1600) 						// expected data 
+		// user: ["superman", "robin", "goblin"], created [1000, 2000)	// other users' data
+		// user: "batman", created : [1000, 1500)						// old data for "batman"
 	}
+
 
 }
